a56adc1fcfdc4846a7d77b7b8c923d08e4c86ab8
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-26 14:19:54 -0300

    criando UsuarioProvider para login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -12,49 +12,47 @@ import {
 
 import { useNavigate } from 'react-router-dom';
 import { UsuarioContext } from 'common/context/Usuario';
+import { useContext } from 'react';
 
 function Login() {
   const navigate = useNavigate();
+  const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext);
   return (
     <Container>
-      <UsuarioContext.Consumer>
-        {({nome, setNome, saldo, setSaldo}) => (
-          <>
-            <Titulo>
-              Insira o seu nome
-            </Titulo>
-            <InputContainer>
-              <InputLabel>
-                Nome
-              </InputLabel>
-              <Input value={nome} onChange={(e) => setNome(e.target.value)}
-                type="text"
-              />
-            </InputContainer>
-            <InputContainer>
-              <InputLabel>
-                Saldo
-              </InputLabel>
-              <Input
-                type="number"
-                value={saldo} onChange={(e) => setSaldo(e.target.value)}
-                startAdornment={
-                  <InputAdornment position="start">
-                    R$
-                  </InputAdornment>
-                }
-              />
-            </InputContainer>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => navigate('/feira')}
-            >
-              Avançar
-            </Button>
-          </>
-        )}
-      </UsuarioContext.Consumer>
+
+      <Titulo>
+        Insira o seu nome
+      </Titulo>
+      <InputContainer>
+        <InputLabel>
+          Nome
+        </InputLabel>
+        <Input value={nome} onChange={(e) => setNome(e.target.value)}
+          type="text"
+        />
+      </InputContainer>
+      <InputContainer>
+        <InputLabel>
+          Saldo
+        </InputLabel>
+        <Input
+          type="number"
+          value={saldo} onChange={(e) => setSaldo(e.target.value)}
+          startAdornment={
+            <InputAdornment position="start">
+              R$
+            </InputAdornment>
+          }
+        />
+      </InputContainer>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => navigate('/feira')}
+      >
+        Avançar
+      </Button>
+
     </Container>
   )
 };
--- src/routes.js
+++ src/routes.js
@@ -4,26 +4,32 @@ import React, { useState } from 'react'
 import Login from 'pages/Login';
 import Feira from "pages/Feira";
 import Carrinho from "pages/Carrinho";
-import { UsuarioContext } from "common/context/Usuario";
+import { UsuarioProvider } from "common/context/Usuario";
 
 export default function AppRoutes() {
-    const [nome, setNome] = useState("");
-    const [saldo, setSaldo] = useState(0);
+
     return (
 
 
-        <UsuarioContext.Provider value={{nome, setNome, saldo, setSaldo}}>
-            <BrowserRouter>
 
-                <Routes>
-                    <Route path="/" element={ <Login />}>
-                       
-                    </Route>
-                    <Route path="feira" element={<Feira />} />
+        <BrowserRouter>
 
-                    <Route path="carrinho" element={<Carrinho />} />
-                </Routes>
-            </BrowserRouter>
-        </UsuarioContext.Provider>
+            <Routes>
+
+                <Route path="/"
+                    element={
+                        <UsuarioProvider>
+                            <Login />
+                        </UsuarioProvider>
+                    } />
+
+
+
+                <Route path="feira" element={<Feira />} />
+
+                <Route path="carrinho" element={<Carrinho />} />
+            </Routes>
+        </BrowserRouter>
+
     )
 }
